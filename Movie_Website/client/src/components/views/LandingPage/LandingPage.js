5498100bfec8db71a6b477b8cb1b277b3b40dadb
MinJae Lee <wjdchs0129@gnu.ac.kr>
2021-12-21 23:27:38 +0900

    Update Files

--- Movie_Website/client/src/components/views/LandingPage/LandingPage.js
+++ Movie_Website/client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config'
+import MainImage from './Sections/MainImage'
 import { FaCode } from "react-icons/fa";
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+    useEffect(() => {
+        const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            setMovies([response.results])
+            setMainMovieImage(response.results[0])
+        })
+    }, [])
+
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style = {{width: '100%', margin: '0'}}>
+            {MainMovieImage && 
+                <MainImage
+                    
+                    image={`${IMAGE_BASE_URL}/w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+
+            <div style = {{width: '85%', margin: '1rem auto'}}>
+                <h2>최신 영화들</h2>
+                <hr />
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+            <div style = {{display: 'flex', justifyContent: 'center'}}>
+                <button>더보기</button>
+            </div>
+        </div>
     )
 }
 
--- Movie_Website/client/src/components/views/NavBar/Sections/LeftMenu.js
+++ Movie_Website/client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -7,16 +7,16 @@ function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
     <Menu.Item key="mail">
-      <a href="/">Home</a>
+      <a href="/">홈</a>
     </Menu.Item>
-    <SubMenu title={<span>Blogs</span>}>
-      <MenuItemGroup title="Item 1">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
+    <SubMenu title={<span>블로그</span>}>
+      <MenuItemGroup title="아이템1">
+        <Menu.Item key="setting:1">옵션1</Menu.Item>
+        <Menu.Item key="setting:2">옵션2</Menu.Item>
       </MenuItemGroup>
-      <MenuItemGroup title="Item 2">
-        <Menu.Item key="setting:3">Option 3</Menu.Item>
-        <Menu.Item key="setting:4">Option 4</Menu.Item>
+      <MenuItemGroup title="아이템2">
+        <Menu.Item key="setting:3">옵션3</Menu.Item>
+        <Menu.Item key="setting:4">옵션4</Menu.Item>
       </MenuItemGroup>
     </SubMenu>
   </Menu>
