b9fd13160d5c133455474741ed4fded9db7f51f8
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-01-02 22:34:56 +0900

    Updating Files(Youtube)

--- Youtube_Website/client/src/components/views/NavBar/Sections/RightMenu.js
+++ Youtube_Website/client/src/components/views/NavBar/Sections/RightMenu.js
@@ -7,38 +7,41 @@ import { withRouter } from 'react-router-dom';
 import { useSelector } from "react-redux";
 
 function RightMenu(props) {
-  const user = useSelector(state => state.user)
+    const user = useSelector(state => state.user)
 
-  const logoutHandler = () => {
-    axios.get(`${USER_SERVER}/logout`).then(response => {
-      if (response.status === 200) {
-        props.history.push("/login");
-      } else {
-        alert('Log Out Failed')
-      }
-    });
-  };
+    const logoutHandler = () => {
+        axios.get(`${USER_SERVER}/logout`).then(response => {
+            if (response.status === 200) {
+                props.history.push("/login");
+            } else {
+                alert('Log Out Failed')
+            }
+        });
+    };
 
-  if (user.userData && !user.userData.isAuth) {
-    return (
-      <Menu mode={props.mode}>
-        <Menu.Item key="mail">
-          <a href="/login">Signin</a>
-        </Menu.Item>
-        <Menu.Item key="app">
-          <a href="/register">Signup</a>
-        </Menu.Item>
-      </Menu>
-    )
-  } else {
-    return (
-      <Menu mode={props.mode}>
-        <Menu.Item key="logout">
-          <a onClick={logoutHandler}>Logout</a>
-        </Menu.Item>
-      </Menu>
-    )
-  }
+    if (user.userData && !user.userData.isAuth) {
+        return (
+            <Menu mode={props.mode}>
+                <Menu.Item key="mail">
+                    <a href="/login">Signin</a>
+                </Menu.Item>
+                <Menu.Item key="app">
+                    <a href="/register">Signup</a>
+                </Menu.Item>
+            </Menu>
+        )
+    } else {
+        return (
+            <Menu mode={props.mode}>
+                <Menu.Item key="upload">
+                    <a href="/video/upload">Video</a>
+                </Menu.Item>
+                <Menu.Item key="logout">
+                    <a onClick={logoutHandler}>Logout</a>
+                </Menu.Item>
+            </Menu>
+        )
+    }
 }
 
 export default withRouter(RightMenu);
---
+++ Youtube_Website/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd'
+import Dropzone from 'react-dropzone'
+
+const { TextArea } = Input
+const { Title } = Typography
+
+function VideoUploadPage() {
+    return (
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <Title level={2}>동영상 업로드</Title>
+            </div>
+
+            <Form onSubmit>
+                <div style={{ display: 'flex', justifyConenter: 'space-between' }}>
+
+                    <Dropzone onDrop multiple maxSize>
+                        {({ getRootProps, getInputProps }) => (
+                            <div style={{
+                                width: '300px', height: '240px',
+                                border: '1px solid lightgrey', display: 'flex',
+                                alignItems: 'center', justifyContent: 'center'
+                            }} {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                <Icon type='plus' style={{ fontSize: '3rem' }} />
+                            </div>
+                        )}
+                    </Dropzone>
+
+                    <div>
+                        <img scr alt />
+                    </div>
+
+                </div>
+
+                <br />
+                <br />
+
+                <label>Title</label>
+                <Input onChange value/>
+                <br />
+                <br />
+
+                <label>Description</label>
+                <TextArea onChange value/>
+                <br />
+                <br />
+
+                <select onChange>
+                    <option key value></option>
+                </select>
+                <br />
+                <br />
+
+                <select onChange>
+                    <option key value></option>
+                </select>
+                <br />
+                <br />
+
+                <Button type="primary" size="large" onClick>
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+export default VideoUploadPage
